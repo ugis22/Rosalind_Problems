e68f90225efe54377c2dd051dfdbb5debcfd6d9e
Maria Eugenia Inzaugarat <Euge@Ugiss-MacBook-Air.local>
2018-01-30 21:39:12 +0100

    Translate to protein

---
+++ Bioinformatics_Stronghold/rna_to_protein.py
@@ -0,0 +1,37 @@
+codon_dictionary = {
+	"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"
+    }
+
+def translate_prot(rna):
+	assert rna[0:3] == 'AUG'
+	protein_syn = ''
+
+	for i in range(0, len(rna)-2, 3):
+		codon = rna[i:i+3]
+		if codon_dictionary[codon] != "STOP":
+			protein_syn += codon_dictionary[codon]
+		else:
+			return protein_syn
+	return protein_syn
+
+
+def read_rna(rna):
+	protein = translate_prot(rna)
+	print(protein)
+
+read_rna('AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA')

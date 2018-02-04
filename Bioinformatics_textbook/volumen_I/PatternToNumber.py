0e5603810f0baff6cb74037b2d691a44ddf1f6ea
Maria eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-02-04 20:30:11 +0100

    Pattern to Number

---
+++ Bioinformatics_textbook/volumen_I/PatternToNumber.py
@@ -0,0 +1,17 @@
+
+def symbol_to_number(symbol):
+	symbols_list = {
+		'A' : 0,
+		'C' : 1,
+		'G' : 2,
+		'T' : 3,
+	}
+	return symbols_list[symbol]
+
+def pattern_to_number(pattern):
+	if len(pattern) == 0:
+		return 0
+	symbol = pattern[-1]
+	prefix = pattern[:-1]
+	
+	return 4*pattern_to_number(prefix)+symbol_to_number(symbol)

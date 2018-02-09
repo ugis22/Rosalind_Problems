7764bef80539c9de514c1d56d76e7c01c9d7ffba
Maria eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-02-10 00:14:09 +0100

    Approximate Match Positions

--- Bioinformatics_textbook/volumen_I/approximate_match_count.py
+++ Bioinformatics_textbook/volumen_I/approximate_match_count.py
@@ -1,6 +1,6 @@
 import HammingDistance
 
-file= open('rosalind_aprox2.txt', 'r')
+file= open('rosalind_ba1h.txt', 'r')
 
 pattern = file.readline().strip()
 print(pattern)
@@ -18,9 +18,7 @@ def approximate_match_positions(pattern, text, d):
 	for i in range(len(text)- m + 1):
 		pattern_current = text[i:i+m]
 		if HammingDistance.hamming_distance(pattern, pattern_current) <= d:
-			positions.append(i)
+			positions.append(str(i))
 	return positions
 
-print(' '.join(map(str, approximate_match_positions(pattern, text, d))))
-
-
+print(' '.join(approximate_match_positions(pattern, text, d)))

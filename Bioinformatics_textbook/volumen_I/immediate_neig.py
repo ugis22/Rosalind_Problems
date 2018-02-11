81433352c731abbf9d19f36ea8918a48366921e6
Maria eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-02-11 20:34:40 +0100

    Immediate neighbors

---
+++ Bioinformatics_textbook/volumen_I/immediate_neig.py
@@ -0,0 +1,9 @@
+def immediate_neig(pattern):
+	nucleotides = ['A', 'C', 'G', 'T']
+	neighborhood = set(pattern)
+	for i in range(len(pattern)):
+		symbol = pattern[i]
+		for j in nucleotides:
+			if j != symbol:
+				neighborhood.append(pattern[:i]+j+[pattern[i+1:]])
+	return neighborhood

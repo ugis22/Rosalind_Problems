633b22508ea5547608788a9b4e3ca2db9c161e1d
Maria eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-02-11 20:48:47 +0100

    Neighbors Code

---
+++ Bioinformatics_textbook/volumen_I/neighboors.py
@@ -0,0 +1,24 @@
+from HammingDistance import hamming_distance
+
+def neighbors(text, d):
+	nucleotides = ['A', 'C', 'G', 'T']
+	if d == 0:
+		return pattern
+	if len(pattern) == 1:
+		return nucleotides
+
+	neighborhood = set()
+	#we generate all the neighbors for (k-1)mer pattern
+	suffix_neig = neighbors(pattern[1:], d)
+
+	for text in suffix_neig:
+		#IF the hamming distance is less than d add any symbol in first position
+		if hamming_distance(pattern[1:], text) < d:
+			for i in nucleotides:
+				neighborhood.append(i+text)
+		#If hamming distance is exactly d add the first symbol of pattern
+		else:
+			neighborhood.append(pattern[0]+text)
+			
+	return neighborhood
+

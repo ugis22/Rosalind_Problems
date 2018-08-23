30c6dcef603d8398c792c10527c5a17edbef7b14
Maria eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-08-23 22:29:10 +0200

    Graph

--- Bioinformatics_textbook/volumen_I/chapter_III/overlap.py
+++ Bioinformatics_textbook/volumen_I/chapter_III/overlap.py
@@ -1,2 +1,14 @@
+class Graph(object):
+
+	def __init__(self, graph_dict=None):
+		if graph_dict == None:
+			graph_dict = {}
+		self.__graph_dict = graph_dict
+		
+	def conection(kmer):
+
+
+
 def graph(kmers):
-	
+
+
